9ac6223ab21bf5fad7cc9f438ff6502811b10b47
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2025-04-20 23:51:18 -0300

    Add files via upload

---
+++ JS/2_variaveis.js
@@ -0,0 +1,27 @@
+const name = 'Luciano';
+
+const lastName = 'Rocha';
+
+const idade = 25;
+
+const valor1 = 2.1;
+
+const valor2 = 3.2;
+
+const frutas = ['maça', 'banana', 'limão', 20, 30, 40, 50.3];
+
+const usuario = {
+    nome: 'Luciano',
+    lastName: lastName,
+    idade: 42,
+};
+
+console.log(usuario.idade);
+
+// console.log('frutas', frutas[0]);
+
+// const isLogin = true;
+
+// console.log(`O valor total é ${valor1 + valor2}`);
+
+// console.log(`Meu nome é ${name} ${lastName} e minha idade é ${idade}`);
